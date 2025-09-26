9403377ffe0fee7820647c68824deb5021d3c2bc
Fathurrohman <fathurrohmanrosyadi@gmail.com>
2025-09-27 05:33:54 +0700

    update

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -19,25 +19,47 @@ export default function Meeting(props: { params: Params }) {
 
   return (
     <div className="w-full h-screen">
-      <JaaSMeeting
-        appId="vpaas-magic-cookie-65612a87d05b4443a0ce23672a0beb8a"
-        roomName={params.id}
-        userInfo={{
-          displayName: user?.fullName || "User",
-          email: user?.primaryEmailAddress?.emailAddress || "",
-        }}
-        spinner={() => (
-          <>
-            <div className="w-full h-screen flex justify-center items-center">
-              <Loader2 className="w-5 h-5 text-gray-600 animate-spin" />
-            </div>
-          </>
-        )}
-        getIFrameRef={(iframe) => {
-          iframe.style.width = "100%";
-          iframe.style.height = "100%";
-        }}
-      />
+      {user && (
+        <>
+          <JaaSMeeting
+            appId="vpaas-magic-cookie-65612a87d05b4443a0ce23672a0beb8a"
+            roomName={params.id}
+            userInfo={{
+              displayName: user!.fullName!,
+              email: user!.primaryEmailAddress!.emailAddress,
+            }}
+            onReadyToClose={() => {
+              location.href = "/";
+            }}
+            configOverwrite={{
+              disableLocalVideoFlip: true,
+              backgroundAlpha: 0.5,
+            }}
+            interfaceConfigOverwrite={{
+              VIDEO_LAYOUT_FIT: "nocrop",
+              MOBILE_APP_PROMO: false,
+              TILE_VIEW_MAX_COLUMNS: 4,
+            }}
+            spinner={() => (
+              <>
+                <div className="w-full h-screen flex justify-center items-center">
+                  <Loader2 className="w-5 h-5 text-gray-600 animate-spin" />
+                </div>
+              </>
+            )}
+            getIFrameRef={(iframe) => {
+              iframe.style.width = "100%";
+              iframe.style.height = "100%";
+            }}
+          />
+        </>
+      )}
+
+      {!user && (
+        <div className="w-full h-screen flex justify-center items-center">
+          <Loader2 className="w-5 h-5 text-gray-600 animate-spin" />
+        </div>
+      )}
     </div>
   );
 }
